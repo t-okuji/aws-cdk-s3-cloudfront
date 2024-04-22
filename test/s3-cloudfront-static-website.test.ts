7ad52a743c655810b5fd8de5d8b1c04a51f3de72
t-okuji <t.okuji34@gmail.com>
2024-04-22 17:09:23 +0900

    First init

---
+++ test/s3-cloudfront-static-website.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3CloudfrontStaticWebsite from '../lib/s3-cloudfront-static-website-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3-cloudfront-static-website-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3CloudfrontStaticWebsite.S3CloudfrontStaticWebsiteStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
